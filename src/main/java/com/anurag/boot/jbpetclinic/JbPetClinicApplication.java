d52693d5dacab09a97e221de5c5eea51aa771ce4
Anurag Chaturvedi <anuragchaturvediknit@gmail.com>
2019-10-05 16:21:50 +0530

    First Commit for Pet clininc appp

---
+++ src/main/java/com/anurag/boot/jbpetclinic/JbPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.anurag.boot.jbpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anurag/boot/jbpetclinic/JbPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anurag.boot.jbpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JbPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
